30c46aec4183be00d08ded49006cf129156fa4be
Max Jung <max.jung97@outlook.de>
2023-12-03 14:30:52 +0100

    added help to 2023 day-3 part-1

--- 2023/day-3/index.ts
+++ 2023/day-3/index.ts
@@ -5,9 +5,42 @@ const lines = prepareLines(data);
 
 let validNumbers: number[] = [];
 
+// TODO: approach that first find all symbols and then check if there is a number next to it
+//
+let partNumberSum = 0;
+
+// my solution: wrong by 49 (probably because of some edge cases)
 function solve(input: string[]) {
     for (let i = 0; i < input.length; i++) {
         const match = input[i].match(/\d+/g);
+
+        const currentLine = input[i];
+        const nextLine = input[i + 1] || "";
+        const previousLine = input[i - 1] || "";
+
+        // part until line 42 comes from internet: https://github.com/lucasteng123/advent-of-code-2023/blob/main/src/days/day3.ts
+        // TODO: find out why this works and my solution from line 45 doesnt
+        const matches = [...currentLine.matchAll(/(\d+)/g)];
+
+        for (const match of matches) {
+            const textBlock = [
+                previousLine.substring(
+                    match.index! - 1,
+                    match.index! + match[0].length + 1
+                ),
+                lines[i].substring(
+                    match.index! - 1,
+                    match.index! + match[0].length + 1
+                ),
+                nextLine.substring(
+                    match.index! - 1,
+                    match.index! + match[0].length + 1
+                ),
+            ];
+
+            const findSymbol = textBlock.join("").match(/[^\d.]/);
+            if (findSymbol) partNumberSum += Number(match[0]);
+        }
 
         if (match) {
             match.forEach((number) => {
@@ -15,16 +48,17 @@ function solve(input: string[]) {
                 const numberEndIndex = numberStartIndex + number.length - 1;
 
                 // check if number has a symbol on the left or right
-                const leftSymbol = input[i].charAt(numberStartIndex - 1);
-                const rightSymbol = input[i].charAt(numberEndIndex + 1);
+                const leftSymbol = input[i]?.charAt(numberStartIndex - 1);
+                const rightSymbol = input[i]?.charAt(numberEndIndex + 1);
 
                 // regex to find symbols except a-z, A-Z, 0-9 and .
-                const regex = /[^0-9.]/;
+                const regex = /[^.\d]/g;
 
                 // check if number has a symbol on the left or right
-                if (leftSymbol.match(regex) || rightSymbol.match(regex)) {
+                if (leftSymbol?.match(regex) || rightSymbol?.match(regex)) {
                     validNumbers.push(parseInt(number));
                     // if one check is true we can skip to the next number in the line
+                    console.log("is valid");
                     return;
                 }
 
@@ -41,6 +75,7 @@ function solve(input: string[]) {
                     if (topSymbol?.match(regex) || bottomSymbol?.match(regex)) {
                         validNumbers.push(parseInt(number));
                         // if one check is true we can skip to the next number in the line
+                        console.log("is valid");
                         return;
                     }
                 }
@@ -51,9 +86,9 @@ function solve(input: string[]) {
 
 solve(lines);
 
-console.log(validNumbers);
+console.log("Part 1 internet", partNumberSum);
 
 console.log(
-    "Part 1",
+    "Part 1 my solution",
     validNumbers.reduce((a, b) => a + b, 0)
 );
