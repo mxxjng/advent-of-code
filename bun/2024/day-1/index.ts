6daeee426737d7f9b05e6024e5c8d05aca6aa551
mxxjng <max.jung97@outlook.de>
2024-12-02 16:37:46 +0100

    feat: added solution for 2024 day-1 part 2

--- bun/2024/day-1/index.ts
+++ bun/2024/day-1/index.ts
@@ -3,23 +3,56 @@ import { loadTextfile, prepareLines } from "@utils/files";
 const data = await loadTextfile("input.txt");
 const lines = prepareLines(data);
 
-let firstArr = [];
-let secondArr = [];
+//const testNumbers = ["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"];
 
-for (const line of lines) {
-    const [first, second] = line.split("   ");
+/**
+ * Generates two arrays from string array
+ * @param lines - lines to generate arrays from
+ * @returns tuple of number arrays
+ */
+function generateNumberArrays(lines: string[]) {
+    let firstArr = [];
+    let secondArr = [];
 
-    firstArr.push(parseInt(first));
-    secondArr.push(parseInt(second));
+    for (const line of lines) {
+        const [first, second] = line.split("   ");
+
+        firstArr.push(parseInt(first));
+        secondArr.push(parseInt(second));
+    }
+
+    return [firstArr, secondArr];
 }
 
-firstArr.sort((a, b) => a - b);
-secondArr.sort((a, b) => a - b);
+function partOne(lines: string[]) {
+    let [firstArr, secondArr] = generateNumberArrays(lines);
 
-let diff = 0;
+    firstArr.sort((a, b) => a - b);
+    secondArr.sort((a, b) => a - b);
 
-for (let i = 0; i < firstArr.length; i++) {
-    diff += Math.abs(firstArr[i] - secondArr[i]);
+    let diff = 0;
+
+    for (let i = 0; i < firstArr.length; i++) {
+        diff += Math.abs(firstArr[i] - secondArr[i]);
+    }
+
+    return diff;
 }
 
-console.log(diff);
+function partTwo(lines: string[]) {
+    let [firstArr, secondArr] = generateNumberArrays(lines);
+
+    let similarityScore = 0;
+
+    for (let i = 0; i < firstArr.length; i++) {
+        let currentNumber = firstArr[i];
+        let occurences = secondArr.filter((n) => n === currentNumber).length;
+
+        similarityScore += currentNumber * occurences;
+    }
+
+    return similarityScore;
+}
+
+console.log(partOne(lines));
+console.log(partTwo(lines));
